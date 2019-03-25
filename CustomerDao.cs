40e450ff88d587be8dd35a5d32edd4b23bd66072
Jeff Richwagen <Jeff Richwagen@DESKTOP-PC0Q50K>
2019-03-25 19:50:01 -0400

    Add project files.

---
+++ CustomerDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MM.DataLayer;
+namespace Adapter
+{
+    public static class CustomerDao
+    {
+        public static List<ICustomer> CustomersByCountry(string country)
+        {
+            List<ICustomer> result = new List<ICustomer>();
+            using (MyModelContainer ctx = new MyModelContainer())
+            {
+                var entityList = ctx.CustomersByCountry(country).ToList();
+                foreach (var entity in entityList)
+                {
+                    result.Add(new CustomersByCountry_Result_Adapter(entity));
+                }
+            }
+            return result;
+        }
+
+        public static List<ICustomer> CustomersWhoBoughtProduct(int productId)
+        {
+            List<ICustomer> result = new List<ICustomer>();
+            using (MyModelContainer ctx = new MyModelContainer())
+            {
+                var entityList = ctx.CustomersWhoBoughtProduct(productId).ToList();
+                foreach (var entity in entityList)
+                {
+                    result.Add(new CustomersWhoBoughtProduct_Result_Adapter(entity));
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = CustomerDao.CustomersByCountry("USA");
+            foreach (var customer in customers)
+                Console.WriteLine(customer.ContactName);
+
+            var moreCustomers = CustomerDao.CustomersWhoBoughtProduct(1);
+                foreach (var otherCustomer in moreCustomers)
+                    Console.WriteLine(otherCustomer.ContactName);
+                Console.ReadLine();
+            }
+
+        }
+    }
+
